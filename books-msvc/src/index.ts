8af9ff18793deb9576fe51652e861ff775a5530c
Ashwanth A R <ashwanth.r@trilogy.com>
2021-10-24 03:54:34 +0530

    iac: add mongodb container (#5)
    * iac: add mongodb container
    
    * fix: lint issues

--- books-msvc/src/index.ts
+++ books-msvc/src/index.ts
@@ -2,10 +2,11 @@ import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server-express';
 import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 
-import { Logger } from '@ashwanth1109/books-catalog-common';
+import { Logger, NodeError, PORT } from '@ashwanth1109/books-catalog-common';
 import { Query, Resolver, buildSchema } from 'type-graphql';
 import express from 'express';
 import http from 'http';
+import mongoose from 'mongoose';
 
 @Resolver()
 class HelloResolver {
@@ -28,10 +29,18 @@ async function main(): Promise<void> {
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
   await server.start();
-  server.applyMiddleware({ app, path: "/api/books/graphql" });
+  server.applyMiddleware({ app, path: '/api/books/graphql' });
   await new Promise((resolve) => {
     httpServer.listen({ port: PORT.BOOKS }, resolve);
   });
+
+  try {
+    await mongoose.connect('mongodb://books-db-svc:27017/books');
+    Logger.info(`Connected to mongodb successfully`);
+  } catch (e: NodeError) {
+    Logger.error(`Error connecting to mongodb: ${e.message}`);
+  }
+
   Logger.info(
     `🚀 Server ready at http://localhost:${PORT.BOOKS}${server.graphqlPath}`
   );
