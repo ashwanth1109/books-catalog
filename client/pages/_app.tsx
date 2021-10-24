1a7534ddbe3d3b307a29b8ebfc7ef4e08e0da100
Ashwanth A R <ashwanth.r@trilogy.com>
2021-10-25 01:26:32 +0530

    feat: use apollo client hooks for gql queries (#11)

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,9 +2,19 @@ import "tailwindcss/tailwind.css";
 import "antd/dist/antd.css";
 
 import type { AppProps } from "next/app";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const client = new ApolloClient({
+    uri: "/api/books/graphql",
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
