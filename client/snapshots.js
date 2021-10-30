107898194f924da1ad2186f00cf1927646e57698
Ashwanth A R <ashwanth1109@gmail.com>
2021-10-31 03:23:25 +0530

    feat: add e2e tests with cypress

---
+++ client/snapshots.js
@@ -0,0 +1,253 @@
+module.exports = {
+  "__version": "8.7.0",
+  "home page e2e": {
+    "should assert that books are listed": {
+      "1": {
+        "data": {
+          "books": [
+            {
+              "_id": "6179a1f524ee6437f46a8eb1",
+              "title": "Things Fall Apart",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8eb2",
+              "title": "Fairy tales",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8eb3",
+              "title": "The Divine Comedy",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8eb4",
+              "title": "The Epic Of Gilgamesh",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8eb5",
+              "title": "The Book Of Job",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8eb6",
+              "title": "One Thousand and One Nights",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8eb7",
+              "title": "Njál's Saga",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8eb8",
+              "title": "Pride and Prejudice",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8eb9",
+              "title": "Le Père Goriot",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8eba",
+              "title": "Molloy, Malone Dies, The Unnamable, the trilogy",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8ebb",
+              "title": "The Decameron",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8ebc",
+              "title": "Ficciones",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8ebd",
+              "title": "Wuthering Heights",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8ebe",
+              "title": "The Stranger",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8ebf",
+              "title": "Poems",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8ec0",
+              "title": "Journey to the End of the Night",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8ec1",
+              "title": "Don Quijote De La Mancha",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8ec2",
+              "title": "The Canterbury Tales",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8ec3",
+              "title": "Stories",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8ec4",
+              "title": "Nostromo",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8ec5",
+              "title": "Great Expectations",
+              "__typename": "Book"
+            }
+          ]
+        }
+      },
+      "2": {
+        "data": {
+          "book": {
+            "_id": "6179a1f524ee6437f46a8eb1",
+            "title": "Things Fall Apart",
+            "description": "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
+            "year": 1958,
+            "__typename": "Book"
+          }
+        }
+      }
+    },
+    "should assert that books app works as expected": {
+      "1": {
+        "data": {
+          "books": [
+            {
+              "_id": "6179a1f524ee6437f46a8eb1",
+              "title": "Things Fall Apart",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8eb2",
+              "title": "Fairy tales",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8eb3",
+              "title": "The Divine Comedy",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8eb4",
+              "title": "The Epic Of Gilgamesh",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8eb5",
+              "title": "The Book Of Job",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8eb6",
+              "title": "One Thousand and One Nights",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8eb7",
+              "title": "Njál's Saga",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8eb8",
+              "title": "Pride and Prejudice",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8eb9",
+              "title": "Le Père Goriot",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8eba",
+              "title": "Molloy, Malone Dies, The Unnamable, the trilogy",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8ebb",
+              "title": "The Decameron",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8ebc",
+              "title": "Ficciones",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8ebd",
+              "title": "Wuthering Heights",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8ebe",
+              "title": "The Stranger",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8ebf",
+              "title": "Poems",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8ec0",
+              "title": "Journey to the End of the Night",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8ec1",
+              "title": "Don Quijote De La Mancha",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8ec2",
+              "title": "The Canterbury Tales",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8ec3",
+              "title": "Stories",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8ec4",
+              "title": "Nostromo",
+              "__typename": "Book"
+            },
+            {
+              "_id": "6179a1f524ee6437f46a8ec5",
+              "title": "Great Expectations",
+              "__typename": "Book"
+            }
+          ]
+        }
+      },
+      "2": {
+        "data": {
+          "book": {
+            "_id": "6179a1f524ee6437f46a8eb1",
+            "title": "Things Fall Apart",
+            "description": "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
+            "year": 1958,
+            "__typename": "Book"
+          }
+        }
+      }
+    }
+  }
+}
